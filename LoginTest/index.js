9cf388529ac691f43ac7a6c30d65ce57dbf2cd68
John <john_giumanca@yahoo.com>
2018-05-14 19:15:44 +0300

    signUP

--- LoginTest/index.js
+++ LoginTest/index.js
@@ -43,3 +43,21 @@ function login(){
 function logout(){
   firebase.auth().signOut();
 }
+
+function signUp(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+
+  window.alert("Error : " + errorMessage);
+  
+});
+
+  window.alert("Hi, your account has been created!");
+
+}
